086db2c6e90bfae2e6ffd46aec51476e93aa091d
Frank Marroquin <frank.marroquin@gmail.com>
2018-05-10 21:19:43 -0500

    Update removevmk1vmk2.ps1

--- removevmk1vmk2.ps1
+++ removevmk1vmk2.ps1
@@ -1,21 +1,18 @@
 # remove vmk1 and 1 from hosts 
-$vmhost_array = @("934249-hyp05.mv.rackspace.com",
-"934251-hyp06.mv.rackspace.com",
-"934252-hyp07.mv.rackspace.com",
-"934253-hyp08.mv.rackspace.com",
-"934255-hyp09.mv.rackspace.com",
-"934256-hyp10.mv.rackspace.com",
-"934257-hyp11.mv.rackspace.com",
-"934258-hyp12.mv.rackspace.com",
-"934259-hyp13.mv.rackspace.com",
-"934262-hyp14.mv.rackspace.com",
-"934265-hyp15.mv.rackspace.com",
-"934266-hyp16.mv.rackspace.com")
+$vmhost_array = @("934249-hyp05.e.com",
+"934251-hyp06.e.com",
+"934252-hyp07.e.com",
+"934253-hyp08.e.com",
+"934255-hyp09.e.com",
+"934256-hyp10.e.com",
+"934257-hyp11.e.com",
+"934258-hyp12.ce.com",
+"934259-hyp13.e.com",
+"934262-hyp14.e.com",
+"934265-hyp15.e.com",
+"934266-hyp16.e.com")
  
- 
-<# $vmhost_array = @("934170-hyp01.mv.rackspace.com",
-"934245-hyp02.mv.rackspace.com",
-"934247-hyp03.mv.rackspace.com") #>
+
 
 
 Foreach ($vmhost in $vmhost_array){
