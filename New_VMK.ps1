d510c443449fbd2b99012a7b6d1d1b677c2cde4e
Frank Marroquin <frank.marroquin@gmail.com>
2018-05-16 16:50:07 -0500

    Create New_VMK.ps1

---
+++ New_VMK.ps1
@@ -0,0 +1,13 @@
+
+$MyFile = Import-CSV ips.csv
+$wchosts = Get-VMHost -State connected | Sort-Object name
+$Westportgroup="Exnet-Prov_Backup_VLAN1027"
+$VS = "4702119-ExNet"
+#$VS = "Management_10dot"
+$SubnetMask = "255.255.255.0"
+$hostarrynumber = 0
+
+ForEach ($i in $MyFile) {
+      New-VMHostNetworkAdapter -VMHost $wchosts[$hostarrynumber] -PortGroup $Westportgroup -VirtualSwitch $VS -IP $i.ips -SubnetMask $SubnetMask
+$hostarrynumber+=1
+    }
