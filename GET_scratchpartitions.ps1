e4ec04757bf09e882af52e651532e4d872ea42bf
Frank Marroquin <frank.marroquin@gmail.com>
2018-07-06 09:23:52 -0500

    Update GET_scratchpartitions.ps1

--- GET_scratchpartitions.ps1
+++ GET_scratchpartitions.ps1
@@ -47,4 +47,6 @@ N is Name and E is Expression (you can also spell out Name and Expression). Spec
 
 You are looking at the short hand for the keys of a calculated property. "N" is short for Name. "L" or Label is also used in place of Name. "E" of course is for Expression.
 They are used when you want to manipulate existing properties or as a simple way to add properties. It is by no means the only way.
-So for our report we are taking the NAME colume and renaming it to esxi and the expression is the name of each host#>
+So for our report we are taking the NAME colume and renaming it to esxi and the expression is the name of each host
+https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-powershell-1.0/ff730948(v=technet.10)
+#>
