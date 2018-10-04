3fa871d687f1bc2e6ef0614c6ef523e6d321cec0
Frank Marroquin <frank.marroquin@gmail.com>
2018-10-04 08:00:50 -0500

    Update Set_FWsyslog.ps1

--- Set_FWsyslog.ps1
+++ Set_FWsyslog.ps1
@@ -1,69 +1,30 @@
-$fwstatus = Import-Csv .\Results.csv
+#$fwstatus = Import-Csv .\Results.csv
+Write-host "This many items in list = " $fwstatus.length 
 $a= $fwstatus.Where({$_.Enabled -eq 'false'})
-
-foreach ($fix in $a ) {
- $ESXfw = (get-esxcli -V2 –vmhost $a.cHost)
- @config
- $ESXFW.network.firewall.rulset.set.Invoke
-
- $ESXFW.network.firewall.refresh.Invoke()
-
-#To modify a host configuration, run the following command to specify the options to change.
-#should be configured already through loginsight
-#esxcli system syslog config set --loghost=tcp|udp|ssl://log_insight-host:514
-#Note You must use udp or tcp, but not both.
-#esxcli system syslog config set --loghost=udp://vrlog.local:514
-#esxcli system syslog config set --loghost=udp://vrlog.local:514
+Write-host "Will Remediate" $a.count "hosts"
+Start-Sleep -s 5
+#$vcServer = VC
+#Connect to vCenter
+Connect-VIServer $vcServer | Out-Null
 
 
-#To ensure that the ESXi firewall is configured to allow syslog traffic to leave the host, run the following commands.
-
-esxcli network firewall ruleset set --ruleset-id=syslog --enabled=true
-esxcli network firewall refresh
-
-#Load the new configuration by running the 
-esxcli system syslog reload
-
+foreach ($fix in $a.chost ) {
+$ESXfw = (get-esxcli -V2 –vmhost $fix)
+$argsb = $ESXFW.network.firewall.ruleset.set.CreateArgs()
+#$args = @(rulesetid, allowedall,enabled )
+$argsb.rulesetid = "syslog"
+#don't need this one. create an error $argsb.allowedall = $true
+$argsb.enabled = $true
+$ESXFW.network.firewall.ruleset.set.Invoke($argsb)
+$ESXfw.network.firewall.refresh.invoke()
+$outmessage = $ESXFW.network.firewall.ruleset.list.Invoke() | where {$_.Name -eq "syslog"}
+write-host $outmessage $fix
 
 }
 
 
+#Disconnect from vCenter
+Disconnect-VIServer $vcServer -Confirm:$false | Out-Null
+   
 
 
-```$obj = New-Object -TypeName PSObject
-    $obj | Add-Member -MemberType NoteProperty -Name 'cHost' -Value  $esxi.name
-    $obj | Add-Member -MemberType NoteProperty -Name 'Service' -Value  $a.Name
-    $obj | Add-Member -MemberType NoteProperty -Name 'Enabled' -Value  $a.Enabled```
-
-That is more or less the same as :
-```$obj = "" | Select cHost, Service, Enabled
-    $obj.cHost = $esxi.name
-    $obj.Service = $a.Name
-    $obj.Enabled = $a.Enabled```
-
-
-
-    
-
-    <#
-    PS C:\Users\fmarr> $fwstatus.Where({$_.Enabled -eq 'false'})
-
-cHost  Service Enabled
------  ------- -------
-hb702  syslog  false
-hb7023 syslog  false
-s10418 syslog  false
-s10410 syslog  false
-s1041  syslog  false
-#>
-
-
-$esxhost = 'hb7022'
-$esxCred = Get-Credential
-#Connect to ESX hosts in cluster
-Connect-VIServer $esxhost -Credential $esxCred | Out-Null
-
-
-
-$ESXfw.ruleset.set ($true, $true, "syslog")
-$ESXFW.network.firewall.rulset.set. ($true, $true, "syslog"0
